6fa2ee63b6738f3879caec226d1f92c96bde0992
thayssal-c <thayssaleiros@gmail.com>
2026-06-10 18:44:16 -0300

    EstudanteEquilibrado.java
    update com super

--- src/character/EstudanteEquilibrado.java
+++ src/character/EstudanteEquilibrado.java
@@ -1,10 +1,33 @@
 package character;
 
 public class EstudanteEquilibrado extends Character {
+    private boolean colaAtiva;
 
     public EstudanteEquilibrado(String nome) {
         super(nome, 100, 15, 5);
+        this.colaAtiva = false;
     }
+
+    @Override
+    public boolean usarSuper() {
+        if (!superDisponivel) return false;
+        superDisponivel = false;
+        colaAtiva = true;
+        return true;
+    }
+
+    public boolean consumirCola() {
+        if (colaAtiva) {
+            colaAtiva = false;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isColaAtiva() { return colaAtiva; }
+
+    @Override
+    public String getNomeSuper() { return "Cola Mental"; }
 
     @Override
     public String getDescricao() {
