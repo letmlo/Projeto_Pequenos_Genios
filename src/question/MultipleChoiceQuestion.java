d97920c2c9cb6192f26cf4743ab5048ab22c52ca
thayssal-c <thayssaleiros@gmail.com>
2026-06-10 18:41:03 -0300

    MultipleChoiceQuestion.java
    update com super

--- src/question/MultipleChoiceQuestion.java
+++ src/question/MultipleChoiceQuestion.java
@@ -1,5 +1,7 @@
 package question;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MultipleChoiceQuestion extends Question {
@@ -25,6 +27,32 @@ public class MultipleChoiceQuestion extends Question {
     @Override
     public boolean verificarResposta(String resposta) {
         return resposta.trim().equalsIgnoreCase(respostaCorreta);
+    }
+
+    @Override
+    public String getDica() {
+        int indiceCorreto = respostaCorreta.charAt(0) - 'A';
+
+        List<Integer> errados = new ArrayList<>();
+        for (int i = 0; i < alternativas.size(); i++) {
+            if (i != indiceCorreto) errados.add(i);
+        }
+        Collections.shuffle(errados);
+
+        List<Integer> eliminados = errados.subList(0, Math.min(2, errados.size()));
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n[COLA MENTAL] Duas alternativas erradas foram eliminadas!\n");
+        char letra = 'A';
+        for (int i = 0; i < alternativas.size(); i++) {
+            if (!eliminados.contains(i)) {
+                sb.append(letra).append(") ").append(alternativas.get(i)).append("\n");
+            } else {
+                sb.append(letra).append(") ██████████ [ELIMINADA]\n");
+            }
+            letra++;
+        }
+        return sb.toString();
     }
 
     public List<String> getAlternativas() {
